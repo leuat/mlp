9123df287a70139cf91e3985ad8fdf309ba52283
Leuat <leuat@irio.co.uk>
2016-06-30 22:11:11 +0200

    wtf

---
+++ Assets/Planet/Scripts/PlanetDesigner.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.IO;
+using System.Text.RegularExpressions;
+using UnityEngine.EventSystems;
+using System.Xml.Serialization;
+
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System.Collections.Generic;
+
+namespace LemonSpawn
+{
+
+   
+
+    public class PlanetDesigner : World
+    {
+
+    	private System.Random r;
+
+    	public override void Start() {
+
+    		base.Start();
+
+    		RenderSettings.MoveCam = false;
+
+
+    	}
+
+		public override void Update() {
+			base.Update();
+
+
+		  }    	
+
+    }
+
+
+}
